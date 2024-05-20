ede1cc21913fc3bbab3856fc988f574949ee47dd
TheMineWay <joelcamposoliva9000@gmail.com>
2024-05-20 12:04:51 +0200

    Install Drizzle

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,9 +1,14 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { neon } from '@neondatabase/serverless';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Env } from '@types-ts/core/env/env.type';
+import { drizzle, NeonHttpDatabase } from 'drizzle-orm/neon-http';
 
 @Injectable()
-export class DatabaseService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
+export class DatabaseService {
+  public readonly db: NeonHttpDatabase;
+  constructor(private readonly configService: ConfigService) {
+    const sql = neon((configService.get('database') as Env['database']).url);
+    this.db = drizzle(sql);
   }
 }
