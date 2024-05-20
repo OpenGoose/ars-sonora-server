4673741d1c9cebfa39e148677eb0b25c4afa9833
TheMineWay <joelcamposoliva9000@gmail.com>
2024-05-20 15:14:56 +0200

    Setup migrations

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,21 +1,25 @@
 import { TABLES_DEFINITION } from '@database/models/tables-definition';
-import { Pool } from '@neondatabase/serverless';
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Env } from '@types-ts/core/env/env.type';
-import { drizzle, NeonDatabase } from 'drizzle-orm/neon-serverless';
+import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
 
 @Injectable()
 export class DatabaseService implements OnModuleInit {
-  public readonly db: NeonDatabase<typeof TABLES_DEFINITION>;
+  public readonly db: PostgresJsDatabase<typeof TABLES_DEFINITION>;
   constructor(readonly configService: ConfigService) {
-    const pool = new Pool({
-      connectionString: (configService.get('database') as Env['database']).url,
-    });
-    this.db = drizzle(pool, { schema: TABLES_DEFINITION, logger: true });
+    const pg = postgres((configService.get('database') as Env['database']).url);
+    this.db = drizzle(pg, { schema: TABLES_DEFINITION, logger: true });
   }
 
   async onModuleInit() {
-    // Migrate
+    return;
+    try {
+      await migrate(this.db, { migrationsFolder: './drizzle' });
+    } catch (e) {
+      Logger.error(e);
+    }
   }
 }
