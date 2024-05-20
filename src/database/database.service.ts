07c52d13a3d3e92d3ad5100e3c25da152bdaf46f
TheMineWay <joelcamposoliva9000@gmail.com>
2024-05-20 12:56:29 +0200

    Setup base for migrations

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,15 +1,19 @@
 import { TABLES_DEFINITION } from '@database/models/tables-definition';
 import { neon } from '@neondatabase/serverless';
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Env } from '@types-ts/core/env/env.type';
 import { drizzle, NeonHttpDatabase } from 'drizzle-orm/neon-http';
 
 @Injectable()
-export class DatabaseService {
+export class DatabaseService implements OnModuleInit {
   public readonly db: NeonHttpDatabase<typeof TABLES_DEFINITION>;
   constructor(readonly configService: ConfigService) {
     const sql = neon((configService.get('database') as Env['database']).url);
     this.db = drizzle(sql, { schema: TABLES_DEFINITION, logger: true });
+  }
+
+  async onModuleInit() {
+    // Migrate
   }
 }
--- src/database/models/common/timestamps.columns.ts
+++ src/database/models/common/timestamps.columns.ts
@@ -2,8 +2,11 @@ import { sql } from 'drizzle-orm';
 import { timestamp } from 'drizzle-orm/pg-core';
 
 export const timestampsDBColumns = {
-  createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .defaultNow()
+    .notNull(),
   updatedAt: timestamp('updated_at', { withTimezone: true })
     .defaultNow()
+    .notNull()
     .$onUpdate(() => sql`now()`),
 };
